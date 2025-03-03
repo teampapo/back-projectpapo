e6e265f3069df9c4c76bb7a6be361f65af57c1cc
GhostDragon <bk.if3@ya.ru>
2025-03-03 18:39:10 +0400

    adding Organization filter
    adding Organization filter and adding email to Customer filter

---
+++ src/main/java/com/example/backprojectpapo/dto/search/OrganizationSearchCriteria.java
@@ -0,0 +1,21 @@
+package com.example.backprojectpapo.dto.search;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrganizationSearchCriteria extends BaseEntitySearchCriteria{
+    private String fullName;
+    private String shortName;
+    private String inn;
+    private String kpp;
+    private String ogrn;
+    private String responsiblePersonSurname;
+    private String responsiblePersonName;
+    private String responsiblePersonPatronymic;
+    private String responsiblePersonEmail;
+    private String responsiblePersonPhoneNumber;
+}
--- src/main/java/com/example/backprojectpapo/repository/OrganizationRepository.java
+++ src/main/java/com/example/backprojectpapo/repository/OrganizationRepository.java
@@ -2,9 +2,10 @@ package com.example.backprojectpapo.repository;
 
 import com.example.backprojectpapo.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
+public interface OrganizationRepository extends JpaRepository<Organization, Integer>, JpaSpecificationExecutor<Organization> {
     Optional<Organization> findByResponsiblePersonEmail(String responsiblePersonEmail);
 }
