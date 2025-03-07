31037a848f289c4d39d047061608db0163de7ef7
Seyrit <seyrit16@mail.ru>
2025-03-07 12:02:48 +0300

    create EmailService

---
+++ src/main/java/com/example/backprojectpapo/service/email/EmailService.java
@@ -0,0 +1,10 @@
+package com.example.backprojectpapo.service.email;
+
+import jakarta.mail.MessagingException;
+
+import java.io.FileNotFoundException;
+
+public interface EmailService {
+    void sendSimpleEmail(String toAddress, String subject, String message);
+    void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment) throws MessagingException, FileNotFoundException;
+}
---
+++ src/main/java/com/example/backprojectpapo/service/email/impl/EmailServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.backprojectpapo.service.email.impl;
+
+import com.example.backprojectpapo.service.email.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileNotFoundException;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender emailSender;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender emailSender){
+        this.emailSender = emailSender;
+    }
+    @Override
+    public void sendSimpleEmail(String toAddress, String subject, String message) {
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(toAddress);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+        emailSender.send(simpleMailMessage);
+    }
+
+    @Override
+    public void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment) throws MessagingException, FileNotFoundException {
+
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+        messageHelper.setTo(toAddress);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(message);
+        FileSystemResource file = new FileSystemResource(ResourceUtils.getFile(attachment));
+        messageHelper.addAttachment("",file);
+        emailSender.send(mimeMessage);
+    }
+}
