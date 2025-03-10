44f7a4bbe5fd1f787a13359218d82a76a8515d01
GhostDragon <bk.if3@ya.ru>
2025-03-10 17:04:25 +0400

    Update ServiceRequestServiceImpl.java

--- src/main/java/com/example/backprojectpapo/service/impl/ServiceRequestServiceImpl.java
+++ src/main/java/com/example/backprojectpapo/service/impl/ServiceRequestServiceImpl.java
@@ -10,10 +10,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ServiceRequestServiceImpl implements ServiceRequestService {
     private ServiceRequestRepository serviceRequestRepository;
     @Override
