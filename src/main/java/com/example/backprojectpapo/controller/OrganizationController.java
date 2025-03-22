f3c05d6c77b0dd378d37cc3fe4bca454779f2d7c
GhostDragon <bk.if3@ya.ru>
2025-03-22 22:09:27 +0400

    adding Delete method to OrganizationController

--- src/main/java/com/example/backprojectpapo/controller/OrganizationController.java
+++ src/main/java/com/example/backprojectpapo/controller/OrganizationController.java
@@ -9,6 +9,7 @@ import com.example.backprojectpapo.dto.response.ServiceRequestOrganizationRespon
 import com.example.backprojectpapo.model.ServiceDetail;
 import com.example.backprojectpapo.service.OrganizationService;
 import com.example.backprojectpapo.service.impl.ServiceDetailServiceImpl;
+import com.example.backprojectpapo.service.web.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,11 +22,13 @@ public class OrganizationController {
 
     private final OrganizationService organizationService;
     private final ServiceDetailServiceImpl serviceDetailService;
+    private final JwtService jwtService;
 
     @Autowired
-    public OrganizationController(OrganizationService organizationService, ServiceDetailServiceImpl serviceDetailService) {
+    public OrganizationController(OrganizationService organizationService, ServiceDetailServiceImpl serviceDetailService, JwtService jwtService) {
         this.organizationService = organizationService;
         this.serviceDetailService = serviceDetailService;
+        this.jwtService = jwtService;
     }
 
     @GetMapping("/get_organization")
@@ -52,6 +55,11 @@ public class OrganizationController {
         ResponseDto<ServiceRequestOrganizationResponseDTO> responseDto = organizationService.getServiceRequestOrganization(token.split(" ")[1]);
         return ResponseEntity.ok().body(responseDto);
     }
-
+    @DeleteMapping()
+    public ResponseEntity<String> deleteOrganization(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        Integer id = jwtService.extractId(token.split(" ")[1]);
+        organizationService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
 }
