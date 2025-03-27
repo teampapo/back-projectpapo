d488e33bb506a1f8b2bc0453388adbae8bdeb230
GhostDragon <bk.if3@ya.ru>
2025-03-27 14:12:09 +0400

    adding service detail linking to organization

--- src/main/java/com/example/backprojectpapo/controller/ServiceDetailController.java
+++ src/main/java/com/example/backprojectpapo/controller/ServiceDetailController.java
@@ -1,6 +1,7 @@
 package com.example.backprojectpapo.controller;
 
 import com.example.backprojectpapo.dto.ResponseDto;
+import com.example.backprojectpapo.dto.request.ServiceDetailPostRequestDTO;
 import com.example.backprojectpapo.dto.request.ServiceDetailPutRequestDTO;
 import com.example.backprojectpapo.dto.response.ServiceDetailResponseDTO;
 import com.example.backprojectpapo.dto.response.ServiceDetailWithOrganizationAllResponseDTO;
@@ -8,6 +9,7 @@ import com.example.backprojectpapo.dto.search.ServiceDetailSearchCriteria;
 import com.example.backprojectpapo.model.ServiceDetail;
 import com.example.backprojectpapo.service.ServiceDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,23 +25,24 @@ public class ServiceDetailController {
     }
 
     @GetMapping("/get_all_services")
-    public ResponseEntity<ResponseDto<ServiceDetailWithOrganizationAllResponseDTO>> getServiceDetailByCriteria(@RequestBody(required = false) ServiceDetailSearchCriteria criteria) {
+    public ResponseEntity<ResponseDto<ServiceDetailWithOrganizationAllResponseDTO>> getServiceDetailByCriteria(@RequestBody(required = false) ServiceDetailSearchCriteria criteria, @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
         if (criteria == null) {
             criteria = new ServiceDetailSearchCriteria();
         }
-        ResponseDto<ServiceDetailWithOrganizationAllResponseDTO> responseDto = serviceDetailService.getAllServiceDetailByCriteria(criteria);
+        ResponseDto<ServiceDetailWithOrganizationAllResponseDTO> responseDto = serviceDetailService.getAllServiceDetailByCriteria(criteria,token.split(" ")[1]);
         return ResponseEntity.ok().body(responseDto);
     }
     @PostMapping()
-    public ResponseEntity<ServiceDetailResponseDTO> createService(@RequestBody ServiceDetail serviceDetail){
-        ServiceDetailResponseDTO responseDTO = serviceDetailService.save(serviceDetail);
+    public ResponseEntity<ServiceDetailResponseDTO> createService(@RequestBody ServiceDetailPostRequestDTO dto, @RequestHeader(HttpHeaders.AUTHORIZATION) String token){
+        ServiceDetailResponseDTO responseDTO = serviceDetailService.save(dto,token.split(" ")[1]);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @PutMapping()
-    public ResponseEntity<ServiceDetailResponseDTO> updateService(@RequestBody ServiceDetailPutRequestDTO dto){
-        return ResponseEntity.ok().body(serviceDetailService.update(dto));
+    public ResponseEntity<ServiceDetailResponseDTO> updateService(@RequestBody ServiceDetailPutRequestDTO dto, @RequestHeader(HttpHeaders.AUTHORIZATION) String token){
+        return ResponseEntity.ok().body(serviceDetailService.update(dto,token.split(" ")[1]));
     }
+
 
     @DeleteMapping()
     public ResponseEntity<String> deleteService(@RequestParam(name = "serviceId") Integer serviceId){
