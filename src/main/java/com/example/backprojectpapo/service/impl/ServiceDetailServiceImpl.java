4173983efe55b6d524c59202ab7486079ef68837
GhostDragon <bk.if3@ya.ru>
2025-04-02 10:37:55 +0400

    Update ServiceDetailServiceImpl.java
    adding cheching for Customer request "Organization and Type of service are required"

--- src/main/java/com/example/backprojectpapo/service/impl/ServiceDetailServiceImpl.java
+++ src/main/java/com/example/backprojectpapo/service/impl/ServiceDetailServiceImpl.java
@@ -136,6 +136,11 @@ public class ServiceDetailServiceImpl implements ServiceDetailService {
         if (jwtData.getRole() == Role.ORGANIZATION){
             criteria.setOrganizationId(jwtData.getId());
         }
+        else if (jwtData.getRole() == Role.CUSTOMER){
+            if (criteria.getOrganizationId() == null || criteria.getTypeId() == null || criteria.getCode() == null || criteria.getName() == null){
+                throw new InvalidRequestException("Organization and Type of service are required");
+            }
+        }
 
         Specification<ServiceDetail> spec = ServiceDetailSpecification.byCriteria(criteria);
         Pageable pageable = PageRequest.of(criteria.getPage(), criteria.getSize());
